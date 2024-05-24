a23aea93e11f873b4ca858e026c98c4042fb6766
Lukasz Kublik <lukasz.kublik.programista@gmail.com>
2024-05-24 06:38:27 +0200

    First run project

---
+++ src/main/java/GraphicalApplication.java
@@ -0,0 +1,16 @@
+package com.example.myfirstgraphicalapp;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GraphicalApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.show();
+    }
+}
