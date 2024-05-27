78e59dc984ffa654bf8ec358b0f2807a178af2d7
Lukasz Kublik <lukasz.kublik.programista@gmail.com>
2024-05-27 06:19:01 +0200

    Set views

--- src/main/java/com/example/myfirstgraphicalapp/GraphicalApplication.java
+++ src/main/java/com/example/myfirstgraphicalapp/GraphicalApplication.java
@@ -2,9 +2,13 @@ package com.example.myfirstgraphicalapp;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class GraphicalApplication extends Application {
 
@@ -17,13 +21,35 @@ public class GraphicalApplication extends Application {
         // Stage -> Scene -> StackPane Text
         primaryStage.show();
         Text text = new Text("Text from first graphical application");
+        Button button = new Button("My fisrt button");
+        Text text2 = new Text("Second text");
 
-        StackPane stackPane = new StackPane(); // root
-        stackPane.getChildren().add(text);
+        // root
+       // StackPane root = new StackPane();
+        // HBox root = new HBox();
+        VBox root = new VBox();
+        root.getChildren().add(text);
+        root.getChildren().add(button);
+        root.getChildren().add(text2);
 
-        Scene scene = new Scene(stackPane);
+        // scene
+        Scene scene = new Scene(root);
+
+        // stage
 
         primaryStage.setScene(scene);
+        primaryStage.setTitle("My App");
+
+        primaryStage.setX(2500);
+        primaryStage.setY(300);
+
+        primaryStage.setWidth(800);
+        primaryStage.setHeight(600);
+
+        //primaryStage.initStyle(StageStyle.DECORATED);
+        //primaryStage.initStyle(StageStyle.UNDECORATED);
+        //primaryStage.initStyle(StageStyle.UTILITY);
+
         primaryStage.show();
     }
 }
