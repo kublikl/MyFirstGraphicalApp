b24ce4f4f457220ec7b161790fcfe78c447caaff
Lukasz Kublik <lukasz.kublik.programista@gmail.com>
2024-05-28 06:09:48 +0200

    My first Control ImgaeView

--- src/main/java/com/example/myfirstgraphicalapp/Controls.java
+++ src/main/java/com/example/myfirstgraphicalapp/Controls.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -26,17 +28,30 @@ public class Controls extends Application {
         label.setLayoutY(35);
         //label.setFont(new Font(20));
         label.setFont(font);
-
         //label.setTextFill(Color.RED);
         label.setTextFill(color);
-
         label.setMaxWidth(50);
         label.setWrapText(true);
         label.setRotate(15);
 
+        // ImageView
+        Image image = new Image("hand.png");
+        ImageView imageView = new ImageView(image);
+       // label.setGraphic(imageView);
+       // Label labelWithImage = new Label("label with image, imageView");
+        imageView.setX(55);
+        imageView.setY(22);
+        imageView.setFitHeight(100);
+        imageView.setFitWidth(200);
+        imageView.setPreserveRatio(true);
 
+
+
+
+/////////////////////////////////////////////////////////////////////////////
         Group group = new Group();
         group.getChildren().add(label);
+        group.getChildren().add(imageView);
 
         Scene scene = new Scene(group, 800,600, Color.WHITE);
 
